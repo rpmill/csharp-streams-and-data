6087eb96b0d9fbd6ac0b781cf9957d2bb0d2a6f1
rpmill <rpmiller@rocketmail.com>
2020-11-02 21:50:02 -0500

    initial commit after creating dotnet console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_streams_and_data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

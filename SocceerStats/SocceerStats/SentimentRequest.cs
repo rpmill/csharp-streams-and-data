f9d660093e55d3c3d394885cb708bc2a563bb464
rpmill <rpmiller@rocketmail.com>
2021-02-16 16:58:01 -0500

    finished course. app is throwing exception in GetSentimentResponse method that needs resolved

---
+++ SocceerStats/SocceerStats/SentimentRequest.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocceerStats
+{
+
+    public class SentimentRequest
+    {
+        [JsonProperty(PropertyName = "documents")]
+        public List<Document> Documents { get; set; }
+    }
+
+    public class Document
+    {
+        public string language { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+    }
+
+}
---
+++ SocceerStats/SocceerStats/SentimentResponse.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocceerStats
+{
+
+    public class SentimentResponse
+    {
+        [JsonProperty(PropertyName = "document")]
+        public List<Sentiment> Sentiments { get; set; }
+        public object[] errors { get; set; }
+        public string modelVersion { get; set; }
+    }
+
+    public class Sentiment
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public string sentiment { get; set; }
+        [JsonProperty(PropertyName = "confidenceScores")]
+        public Confidencescores ConfidenceScores { get; set; }
+        public Sentence[] sentences { get; set; }
+        public object[] warnings { get; set; }
+    }
+
+    public class Confidencescores
+    {
+        [JsonProperty(PropertyName = "positive")]
+        public double Positive { get; set; }
+        [JsonProperty(PropertyName = "neutral")]
+        public double Neutral { get; set; }
+        [JsonProperty(PropertyName = "negative")]
+        public double Negative { get; set; }
+    }
+
+    public class Sentence
+    {
+        public string sentiment { get; set; }
+        public Confidencescores1 confidenceScores { get; set; }
+        public int offset { get; set; }
+        public int length { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Confidencescores1
+    {
+        public float positive { get; set; }
+        public float neutral { get; set; }
+        public float negative { get; set; }
+    }
+
+}
